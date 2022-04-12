b421d09e1c1952f8fe6295de23005773bb81f2c6
JessaTehCrow <jellekok.3.4@gmail.com>
2022-04-12 21:02:30 +0200

    Comments to highlight & suggestion

--- utils/highlight.py
+++ utils/highlight.py
@@ -1,9 +1,9 @@
-from utils.cprint import cconvert, escape
 from utils.shell import function
 from utils.utils import types
 
 import os
 
+# Default color scheme
 colors = {
     "text_color":'[E]',
     "command" : "[B]",
@@ -22,68 +22,92 @@ class Highlight():
     def __init__(self, shell):
         self.shell = shell
         self.modules = []
+        
+        # Load custom color scheme
         self.colors = shell.loader.load(colors, "colors", "shell")
     
     def _get_functions(self, module:str):
+        # Load all functions of modules from shell
         return {k:v for k,v in self.shell.commands[module].items() if isinstance(v, function)}
 
-
+    # Get all modules from shell
     def _update_data(self):
         self.modules = list(self.shell.commands)
 
-
+    # Here's where the fun 'idk what the fuck this' is happens
     def _get_args(self, module:str, function:str, args:list):
+        # Prepare a list as long as the arguments
         cols = [None]*len(args)
 
         file = module
+
+        # if the command doesn't exist, ignore this pass
         if not module in self.shell.commands or not function in self.shell.commands[module]:
             return
-
+        
+        # If a custom highlighting function is available, use that
         if hasattr(self.shell.raw_import[file], f"_{function}_highlight"):
             return getattr(self.shell.raw_import[file], f"_{function}_highlight")(self.shell, args)
 
+        # get function args
         f_args:list = self.shell.commands[module][function].args
 
+        # Loop through function args
         for i, f_arg in enumerate(f_args):
-            if i >= len(args):
+            if i >= len(args): # If more function args than passed args, continue to next
                 break
-
+            
+            # Get argument type (int, str, etc.)
             arg_type = f_arg[1]
             color = self.colors['text_color']
 
+            # get the color for the argument
             if arg_type and arg_type+"_arg" in self.colors:
                 correct, _ = types[arg_type](args[i])
 
                 color = self.colors[arg_type+"_arg"]
                 if not correct:
                     color = self.colors['wrong_arg']
-
+            
+            # Set the color
             cols[i] = color
         
+        # Loop through 
         for i, arg in enumerate( args[len(f_args):] ):
-            color = self.colors['wrong_arg']
+            color = self.colors['wrong_arg'] # default color
             
+            # If it's an starred argument, 
             if len(f_args) and f_args[-1][0].startswith("*"):
                 l_arg = f_args[-1]
 
+                # If the function argument does not have any associated datatype, make the color text color
                 if l_arg[1] == '_empty':
                     color = self.colors['text_color']
 
                 else:
+                    # Very cryptic, but all it does is see if the user argument is the same datatype as the function argument
                     correct, _ = types[l_arg[1]] (args[i+len(f_args)])
                     
                     if correct:
                         color = self.colors[l_arg[1]+"_arg"]
+                        # Get color for thing
             
+            # Set color
             cols[i+len(f_args)] = color
         
+        # Return colors
         return cols
 
-
+    # More cryptic fun
     def _get_highlight(self, command:list):
+        # copy command
         command_copy = list(command)
+
+        # Prepare color array
         cols = [self.colors["text_color"]]*len(command)
         
+        # default values
+        args_offset = 2
         module = command_copy[0]
         func = None
 
@@ -96,25 +120,24 @@ class Highlight():
             cols[0] = self.colors["file_color"]
             return cols
 
-        # sub-command
+        # if there's at least 2 items in the split command, and the second entry is a sub-function for the module
         if len(command) > 1 and command_copy[0] in self.modules and command[1] in self._get_functions(command_copy[0]):
             func = command_copy[1]
             cols[1] = self.colors["sub_command"]
         
-        # get args
+        # if there's at least 2 items in the split command, and `main` in the module and the second entry in the split command is not a sub-function, parse it as arguments for main
         if len(command) > 1 and (module in self.modules) and ('main' in self._get_functions(module)) and (func not in self._get_functions(module)):
             func = "main"
             args_offset = 1 
-
-        else:
-            args_offset = 2
         
+        # Get argument highlight
         args = command[args_offset:]
         args = self._get_args(module, func, args)
 
         if args:
             cols[args_offset:] = args
         
+        # Return colors
         return cols
         
     def apply(self, command:list):
--- utils/suggestion.py
+++ utils/suggestion.py
@@ -6,17 +6,19 @@ class Suggestion():
         self.shell = shell
         self.modules = []
     
+    # get all functions from modules
     def _get_functions(self, module:str):
         return {k:v for k,v in self.shell.commands[module].items() if isinstance(v, function)}
 
-
+    # Get all modules
     def _update_data(self):
         self.modules = list(self.shell.commands)
 
-
+    # Get suggestions for functions
     def _get_args(self, module:str, function:str, args:list):
         filename =  module
 
+        # If there's a custom suggestion function available, use that
         if hasattr(self.shell.raw_import[filename], f"_{function}_suggestion"):
             custom_sugg = getattr(self.shell.raw_import[filename], f"_{function}_suggestion")(self.shell, args)
 
@@ -24,10 +26,14 @@ class Suggestion():
                 return custom_sugg
 
         f_args = self.shell.commands[module][function].args
+
+        # Get names for arguments and show if optional or not
         out = [f"{'!' if x[2] == 'None' else ''}{x[0]}" for x in f_args[len(args):]]
+        
+        # join it all together (but don't allow tab complete)
         return False, ' '+', '.join(out)
 
-
+    # Get sub-function suggestions
     def _get_sub(self, module:str, args:list):
         funcs = self._get_functions(module)
 
@@ -35,36 +41,51 @@ class Suggestion():
             return self._get_args(module, 'main', args)
         
         elif len(args) <= 1:
+            # get args
             arg = args[0] if len(args) else ''
-            space = '' if arg else ' '
 
+            # Don't know how to explain this- but blah blah, empty spot unless already typed
+            space = '' if arg else ' '
+            
+            # Get anything you can suggest into
             closest = [x for x in funcs if x.startswith(arg)]
             
+            # IF there's anything that you *CAN* autocomplete into, suggest that
             if closest:
                 return True, f"{space}{closest[0][len(arg):]}"
 
     
     def _get_suggestion(self, command:list):
         cmd = command[0]
+        
+        # if command is not a module
         if len(command) == 1 and not cmd in self.modules:
+            # Get all modules that start with the current cmd input
             commands = [x for x in self.modules if x.startswith(cmd)]
+            
+            # Get all files in current directory that start with the current cmd input
             file = [x for x in os.listdir() if os.path.isfile(x) and x.startswith(cmd)]
             
+            # If there's a command you could complete into, suggest that over files, and allow it to be tab-completed
             if commands:
                 return True, commands[0][len(cmd):]
 
+            # If there's no possible command, suggest a file (if it exists) and allow it to be tab-completed
             elif file:
                 return True, file[0][len(cmd):]
-            
+        
+        # If there's more than 1 argument (or cmd matched a module)
         elif len(command) >= 1:
+            # Get sub command
             sub = command[1] if command[1:] else ""
 
+            # If cmd is a module, and the second cmd entry is not a valid sub-function, parse sub function
             if cmd in self.modules and not sub in self._get_functions(cmd):
                 return self._get_sub(cmd, command[1:])
             
+            # If cmd is a module, and the second cmd entry is a sub function, parse arguments
             elif cmd in self.modules and sub in self._get_functions(cmd):
                 return self._get_args(cmd, sub, command[2:])
-
 
     def get(self, command:list):
         if not command:
